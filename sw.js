2efc01e485a415e58477430a1ad42520e86c0dcb
AHAMEDHASSAN <  GitHub>
2026-02-18 06:08:05 +0200

    update

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const ASSETS_TO_CACHE = [
     './search.js',
     './settings.js',
     './manifest.json',
-    './img/Logo.jpeg'
+    './img/icon-512.png'
 ];
 
 // Install Event: Pre-cache core assets
